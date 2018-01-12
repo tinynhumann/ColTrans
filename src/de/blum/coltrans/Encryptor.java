b17b9922c49c5a13c08f295fb7a02b1e8c65315c
Oliver Blum <oliver.blum1987@googlemail.com>
2018-01-12 14:20:46 +0100

    add comments

--- src/de/blum/coltrans/Encryptor.java
+++ src/de/blum/coltrans/Encryptor.java
@@ -1,9 +1,16 @@
 package de.blum.coltrans;
 
+/**
+*
+* @author oliver
+*/
 public class Encryptor {
 	
 	public static String encrypt(String text, int[] key) {
 		
+		/*
+		 * extend text with '_', so it can be divided through key length.
+		 */
 		int mod = key.length - text.length() % key.length;
 		for(int i = 0; i < mod; i++) {
 			text += "_";
@@ -12,11 +19,15 @@ public class Encryptor {
 		char[] plain = text.toCharArray();
 		char[] secret = new char[plain.length];
 		
+		/*
+		 * sort text characters given by key
+		 */
 		int j = 0;
 		for(int i = 0; i < plain.length; i++) {
 			secret[i] = (char) plain[key[j]];
 			j++;
 			if(j == key.length) {
+				//update key values to new text indices after all transposition are done.
 				for (int k = 0; k < key.length; k++) {
 					key[k] += key.length;
 				}
@@ -26,6 +37,9 @@ public class Encryptor {
 		
 		char[][] coltrans = new char[(secret.length / key.length)][key.length];
 		
+		/*
+		 * write secret into two-dimensional array with x rows and y columns
+		 */
 		int i = 0;
 		for (int x = 0; x < coltrans.length; x++) {
 			for (int y = 0; y < coltrans[0].length; y++) {
@@ -36,6 +50,9 @@ public class Encryptor {
 			}
 		}
 		
+		/*
+		 * read from this two-dimensional array with y rows and x columns.
+		 */
 		i = 0;
 		secret = new char[((secret.length / key.length)) * key.length];
 		for (int x = 0; x < coltrans[0].length; x++) {
